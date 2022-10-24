b9e5b12a6a8ae5fcd436c4a8d2d3d81e7691710a
Moni <moni@Monis-MacBook-Pro.local>
2022-10-24 13:22:23 +0300

    Added traffic to the web server

--- main.tf
+++ main.tf
@@ -10,85 +10,16 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-# 1.Create VPC
-resource "aws_vpc" "prod-vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    "Name" = "Production"
-  }
-}
-# 2.Create Internet Gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.prod-vpc.id
-}
-
-
-# 4.Create a custom route table
-resource "aws_route_table" "prod-route-table" {
-  vpc_id = aws_vpc.prod-vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  route {
-    ipv6_cidr_block = "::/0"
-    gateway_id      = aws_internet_gateway.gw.id
-  }
-
-  tags = {
-    Name = "Production"
-  }
-}
-
-
-
-# 3.Create a subnet
-resource "aws_subnet" "subnet-1" {
-  vpc_id            = aws_vpc.prod-vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    "Name" = "Production-subnet"
-  }
-}
-
-# 4.Associate subnet with route table
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.subnet-1.id
-  route_table_id = aws_route_table.prod-route-table.id
-}
-
-# 5.Create security group to allow following PORT (22,80,443)
-resource "aws_security_group" "allow_web" {
-  name        = "allow_web_traffic"
-  description = "Allow web inbound traffic"
-  vpc_id      = aws_vpc.prod-vpc.id
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+ 
 
   ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
   }
 
   egress {
@@ -100,27 +31,8 @@ resource "aws_security_group" "allow_web" {
   }
 
   tags = {
-    Name = "allow_WEB"
+    Name = "allow_tls"
   }
-}
-
-# 6.Create a network interface with an IP that was created in step 4.
-
-resource "aws_network_interface" "web-server-net-interface" {
-  subnet_id       = aws_subnet.subnet-1.id
-  private_ips     = ["10.0.1.50"]
-  security_groups = [aws_security_group.allow_web.id]
-
-
-}
-# 7.Assaign an EIP & attach to IG
-
-resource "aws_eip" "one" {
-  vpc                       = true
-  network_interface         = aws_network_interface.web-server-net-interface.id
-  associate_with_private_ip = "10.0.1.50"
-  depends_on                = [aws_internet_gateway.gw]
-
 }
 
 # 8.Create Ubuntu server and install/enable apache 2.
@@ -130,23 +42,20 @@ resource "aws_instance" "Thesis_server" {
   availability_zone = "us-east-1a"
   key_name          = "web-server-key-pair"
 
-  network_interface {
-    device_index         = 0
-    network_interface_id = aws_network_interface.web-server-net-interface.id
-  }
+ 
 
 
   user_data = <<-EOF
-               #!/bin/bash
-yum -y install httpd
-systemctl enable httpd
-systemctl start httpd
-echo '<html><h1>Hello From Your Web Server!</h1></html>' > /var/www/html/index.html
+          #!/bin/bash
+          yum -y install httpd
+          systemctl enable httpd
+          systemctl start httpd
+          echo '<html><h1>Hello From Your Web Server!</h1></html>' > /var/www/html/index.html
               EOF
 
 
 
   tags = {
-    Name = "Terraform Lab"
+    Name = "Lab"
   }
 }
